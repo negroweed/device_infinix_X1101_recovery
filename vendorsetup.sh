79204d5229d7456258e6f2a9ebf984e9a48852c7
negroweed <134845563+negroweed@users.noreply.github.com>
2024-11-22 09:02:25 +0000

    Revert "XPAD: Nuke vibrator changes"
    This reverts commit c35ae53d66d5377e434c6ea5df34f5cf9219789f.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 export LC_ALL="C"
+cd bootable/recovery
+
+for p in ../../device/infinix/Infinix-XPAD/patches/*; do
+if ! git am -3 <$p; then
+    # Force use fuzzy patch
+    patch -p1 <$p
+    git add .
+    git am --continue || {
+        echo "Failed to apply $p"
+        exit 1
+    }
+fi
+done
+
+cd ../../
